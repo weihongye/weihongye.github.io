8f7223f6129ffaea102d6d0b1dcc418cff3e1816
weihongye <weihongye@xiaomi.com>
2017-09-30 19:44:51 +0800

    新增：素材红色的叶子等

--- dai/dai/js/detect.js
+++
@@ -1,6 +0,0 @@
-if (/nokia|iphone|android|ipad|ipod|windows phone/i.test(navigator.userAgent)) {
-    var strUrl = window.location.href,
-        arrUrl = strUrl.split("/"),
-        strPage = arrUrl[arrUrl.length - 1];
-    window.location.href = "m/"+strPage + window.location.search;
-}
